79c799170e5f741238ddd3cfe67f5c411d820d5a
Ohad Arazi <Ohad.a10@gmail.com>
2020-02-09 16:51:45 +0200

    Init comit

---
+++ public/js/app.js
@@ -0,0 +1,23 @@
+// console.log('Client side JS file is loaded!')
+
+const weatherForm = document.querySelector('form')              
+const search = document.querySelector('input')                   
+const messageOne = document.querySelector('#message-1')       
+const messageTwo = document.querySelector('#message-2')                 
+
+weatherForm.addEventListener('submit', (e) => {   // Event listener for on click 
+    e.preventDefault()                           //  Avoid the function to re-load the page by default
+    messageOne.textContent = "LOADING..."       
+    messageTwo.textContent = ""                 //   Clean the <p> from last search resault
+    const location = search.value              //    Geting the location value from the user's imput
+    fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {   // Fetch the resault from the query 
+    response.json().then((data) => {
+        if (data.error) {
+            messageOne.textContent = data.error      // Printing error if needed
+        } else {
+            messageOne.textContent = data.location  //  Printing resaulr
+            messageTwo.textContent = data.forecast
+        } 
+    })
+})
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,16 @@
+const request = require('request')
+
+const forecast = ({latitude, longtitude, location}, callback) => {
+    const url = 'https://api.darksky.net/forecast/c945e439c407735fe6f0f90a07c37894/' + latitude + ',' + longtitude + '?units=si'
+    request({url, json: true}, (error, {body} = {}) => {
+        if (error) {
+            callback('Unable to connect to weather srvice!', undefined)
+        } else if (body.error) {
+            callback('Unable to find location!', undefined)
+        } else {
+            callback( undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability}% chance of rain!`)
+        }
+    })
+}
+
+module.exports = forecast
