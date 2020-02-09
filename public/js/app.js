afca20058b006d556c085f322164edf9da3056ee
Ohad Arazi <Ohad.a10@gmail.com>
2020-02-09 18:54:38 +0200

    Comments delete

--- public/js/app.js
+++ public/js/app.js
@@ -3,17 +3,17 @@ const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')       
 const messageTwo = document.querySelector('#message-2')                 
 
-weatherForm.addEventListener('submit', (e) => {   // Event listener for on click 
-    e.preventDefault()                           //  Avoid the function to re-load the page by default
+weatherForm.addEventListener('submit', (e) => {   
+    e.preventDefault()                           //  Avoid func re-load the page by default
     messageOne.textContent = "LOADING..."       
     messageTwo.textContent = ""                 //   Clean the <p> from last search resault
     const location = search.value              //    Geting the location value from the user's imput
     fetch(`/weather?address=${location}`).then((response) => {   // Fetch the resault from the query 
     response.json().then((data) => {
         if (data.error) {
-            messageOne.textContent = data.error      // Printing error if needed
+            messageOne.textContent = data.error      
         } else {
-            messageOne.textContent = data.location  //  Printing resaulr
+            messageOne.textContent = data.location  
             messageTwo.textContent = data.forecast
         } 
     })
