0245c160bc77f152b82481fae0a3e9908bfce125
Ohad Arazi <Ohad.a10@gmail.com>
2020-02-11 02:46:23 +0200

    forecast message fix

--- public/js/app.js
+++ public/js/app.js
@@ -1,20 +1,23 @@
 const weatherForm = document.querySelector('form')              
 const search = document.querySelector('input')                   
-const messageOne = document.querySelector('#message-1')       
-const messageTwo = document.querySelector('#message-2')                 
+const messages = document.getElementsByTagName('p')                        
 
 weatherForm.addEventListener('submit', (e) => {   
     e.preventDefault()                           //  Avoid func re-load the page by default
-    messageOne.textContent = "LOADING..."       
-    messageTwo.textContent = ""                 //   Clean the <p> from last search resault
+    messages[0].textContent = "LOADING..."       
+    messages[1].textContent = ""                 //   Clean the <p> from last search resault
     const location = search.value              //    Geting the location value from the user's imput
     fetch(`/weather?address=${location}`).then((response) => {   // Fetch the resault from the query 
     response.json().then((data) => {
         if (data.error) {
             messageOne.textContent = data.error      
         } else {
-            messageOne.textContent = data.location  
-            messageTwo.textContent = data.forecast
+            const forecastStr = data.forecast.split("!")
+            messages[0].textContent = data.location
+            
+            for (let i=0; i <= messages.length; i++){
+                messages[i+1].textContent = forecastStr[i] 
+            }
         } 
     })
 })
